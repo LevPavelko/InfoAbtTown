a70cac73989601bda1b33165eaff7dc82b4abab7
LevPavelko <lev.pavelko.studies@gmail.com>
2024-05-27 11:04:01 +0200

    Task3: Инициализация и стилизация достопримечательностей

--- MainSight.jsx
+++ MainSight.jsx
@@ -1,3 +1,15 @@
-function MainSight(){
-    return <h2>Главная</h2>;
+class MainSight extends React.Component{
+    render(){
+        return (
+            <div className="centerDiv">
+                <h1 style={{fontSize: 40, margin: 10, padding: 10}}>{this.props.header}</h1>
+               
+                <img src={this.props.img} className="imgOfSight"></img>
+                <p style={{fontSize: 28,padding: 10}}>{this.props.text}</p>
+            </div>
+        
+    
+        );
+    }
+    
 }
--- OtherSight.jsx
+++ OtherSight.jsx
@@ -1,3 +1,24 @@
-function OtherSight(){
-    return <h2>Главная</h2>;
+class OtherSight extends React.Component{
+    render(){
+        return (
+            <div className="centerDiv">
+                <h1 style={{fontSize: 40, margin: 10, padding: 10}}>{this.props.headerHofburg}</h1>
+                <img src={this.props.imgOfHofburg} className="imgOfSight"></img>
+                <p style={{fontSize: 28,padding: 10}}>{this.props.descriptionOfHofburg}</p>
+
+
+                <h1 style={{fontSize: 40, margin: 10, padding: 10}}>{this.props.headerBelvedere}</h1>
+               <img src={this.props.imgBelvedere} className="imgOfSight" style={{width: 730, height: 455}}></img>
+               <p style={{fontSize: 28,padding: 10}}>{this.props.descriptionOfBelvedere}</p>
+
+               <h1 style={{fontSize: 40, margin: 10, padding: 10}}>{this.props.headerSchönbrunn}</h1>
+               <img src={this.props.imgOfSchönbrunn} className="imgOfSight" style={{width: 730, height: 455}}></img>
+               <p style={{fontSize: 28,padding: 10}}>{this.props.descriptionOfSchönbrunn}</p>
+
+            </div>
+        
+    
+        );
+    }
+    
 }

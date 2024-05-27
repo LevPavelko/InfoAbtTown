4564322b0f8e6b405970c589af4b87ef270d1ed1
LevPavelko <lev.pavelko.studies@gmail.com>
2024-05-27 16:12:07 +0200

    Done

--- Images.jsx
+++ Images.jsx
@@ -1,3 +1,16 @@
-function Images(){
-    return <h2>Главная</h2>;
+class Images extends React.Component{
+    render(){
+        return (
+            <div className="centerDiv">
+               
+               <h1 style={{fontSize: 40, margin: 10, padding: 10}}>Pictures of Vienna</h1>
+                <img src={this.props.img1} className="imgOfSight" style={{margin: 15}}></img>
+                <img src={this.props.img2} className="imgOfSight" style={{margin: 15}}></img>
+                <img src={this.props.img3} className="imgOfSight" style={{margin: 15}}></img>
+                <img src={this.props.img4} className="imgOfSight" style={{margin: 15}}></img>
+                <img src={this.props.img5} className="imgOfSight" style={{margin: 15}}></img>
+            </div>
+        );
+    }
+    
 }

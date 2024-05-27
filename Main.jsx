99ae87b89e92b31f9f837b873f501afcd68f62b2
LevPavelko <lev.pavelko.studies@gmail.com>
2024-05-27 10:18:55 +0200

    Task1: Стилизация и инициализация главной страницы

--- Main.jsx
+++ Main.jsx
@@ -1,3 +1,13 @@
-function Main(){
-    return <h2>Главная</h2>;
+class Main extends React.Component{
+    
+    render(){
+        return (
+            <div className="centerDiv">
+                <h1 style={{fontSize: 42, margin: 0, padding: 0}}>Vienna</h1>
+                <h1 style={{fontSize: 30}}>{this.props.text}</h1>
+                <img src={this.props.img} className="mainImg"></img>
+               
+            </div>
+        );
+    }
 }
